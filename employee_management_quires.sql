58a2327b48fb67d17bcde2f3c030517cb5007d98
rfi33 <hei.finoana.5@gmail.com>
2025-11-12 12:49:11 +0300

    feat: add queries for exercises 5 and 6

--- employee_management_quires.sql
+++ employee_management_quires.sql
@@ -1,15 +1,23 @@
-select employee.first_name,employee.last_name from employee 
-left join in employee.id_team = team.id where(id_team is null);
+SELECT employee.first_name,employee.last_name FROM employee 
+LEFT JOIN team on employee.id_team = team.id WHERE(id_team is null);
 
+SELECT employee.first_name,employee.last_name FROM leave RIGHT JOIN employee 
+ON leave employee on leave.id_employee = employee.id WHERE (id_team is null);
 
-select employee.first_name,employee.last_name from leave right join employee 
-on leave employee on leave.id_employee = employee.id where(id_team is null);
+SELECT leave.id,leave.start_date,leave.end_date,employee.first_name,employee.last_name,
+team.name FROM leave INNER JOIN employee ON leave.id_employee = employee.id
+LEFT JOIN team ON employee.id_team = team.id;
 
+SELECT employee.contract_type, COUNT(employee.contract_type) AS employee_count FROM 
+employee GROUP BY (employee.contract_type); 
 
-select leave.id,leave.start_date,leave.end_date,employee.first_name,employee.last_name,
-team.name from leave inner join employee on leave.id_employee = employee.id
-left join team on employee.id_team = team.id;
+SELECT COUNT(DISTINCT leave.id_employee) AS number_of_employees_on_leave FROM employee
+INNER JOIN leave ON leave.id_employee = employee.id
+WHERE (NOW()>=leave.start_date
+AND NOW()<= leave.end_date);
 
-
-select employee.contract_type, count(employee.contract_type) as employee_count from 
-employee group by (employee.contract_type); 
+SELECT employee.id ,employee.first_name , employee.last_name , team.name AS team_name FROM employee
+INNER JOIN leave ON leave.id_employee = employee.id
+INNER JOIN team ON  employee.id_team = team.id
+WHERE (NOW()>=leave.start_date
+  AND NOW()<= leave.end_date);
